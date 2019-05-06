d44389822be50e46002abed04d00cd581959daa1
elieu17 <44182844+elieu17@users.noreply.github.com>
2019-05-06 19:26:04 -0400

    Update logic.js

--- Price and Crime_Map/static/js/logic.js
+++ Price and Crime_Map/static/js/logic.js
@@ -52,7 +52,7 @@ function createMap(listings) {
   var map = L.map("map", {
     center: [40.7128, -74.0059],
     zoom: 11,
-    layers: [streetmap, price_layer]
+    layers: [streetmap]
   });
 
   ////////////////////////////////////////////////////////////////////////
